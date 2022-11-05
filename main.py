21d552b9cef9fbc4e3ffa16bbc160841a66b7461
irynagrape <109566237+irynagrape@users.noreply.github.com>
2022-11-05 19:45:40 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyqrcode
+
+from PIL import Image
+
+link = input("Enter anything to generate QR : ")
+
+qr_code = pyqrcode.create(link)
+
+qr_code.png("QRCode.png", scale=5)
+
+Image.open("QRCode.png")
